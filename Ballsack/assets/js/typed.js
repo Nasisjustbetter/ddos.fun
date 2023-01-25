89f006f98b04da955af53d6db49265abc258a163
Nasisjustbetter <123530972+Nasisjustbetter@users.noreply.github.com>
2023-01-24 22:43:47 -0600

    Add files via upload

---
+++ Ballsack/assets/js/typed.js
@@ -0,0 +1,8 @@
+new Typed('#typed',{
+    strings : [,'+_=#9908','larp', 'horny', 'skid', 'manipulator', 'loser', 'nerd', 'monkey',],
+    typeSpeed : 40,
+    delaySpeed : 90,
+    showCursor: false,
+    loop : true
+  })
+  
